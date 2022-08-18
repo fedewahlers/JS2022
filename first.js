5d5d0fecd1b50148e54952fab65270d9f2202e74
fedewahlers <fedewahlers@gmail.com>
2022-08-18 20:04:20 -0300

    first

---
+++ first.js
@@ -0,0 +1,12 @@
+//CREO VARIABLES Y LAS UITILIZO JUNTO AL ESPECTADOR//
+
+let Uno =  prompt ("Ingresa tu nombre")
+let mensaje  = alert ("Bienvenido/a "+ Uno)
+let Edad = prompt ("Ingresa tu edad") 
+
+//APLICO CONDICIONAL IF Y ELSE//
+
+let numero = 18;
+
+if (Edad >= numero) { alert ("Perfecto, sos mayor de edad, ingreso exitoso ")} 
+else if (Edad <= numero){ alert ("Lo siento solo se permite el ingreso a mayores de 18")}  
